807f80f233bdada708b9b0cc42737071225acc5a
SHAIKH SOHEL SHAIKH SHAKIL <91606990+sohelshekhatik1998@users.noreply.github.com>
2022-10-31 16:37:43 +0530

    schema
    schema

--- user.py
+++ user.py
@@ -1,37 +1,6 @@
-from fastapi import APIRouter
-from config.db import conn
-from model.index import users
-from schema.index import User
+from pydantic import BaseModel
 
-user = APIRouter
-
-@user.get("/")
-async def read_data():
-    return conn.execute(users.select()).fetchall()
-
-@user.get("/{id}")
-async def read_data(id: int):
-    return conn.execute(users.select().where(user.c.id==id)).fetchall()
-
-@user.post("/")
-async def write_data(users:User):
-    conn.execute(users.insert().values(
-        name=user.name,
-        email=user.email,
-        passward=user.passward
-    ))
-
-@user.get("{id}")
-async def update_data(id: int, users:User):
-    conn.execute(users.update().values(
-        name=user.name,
-        email=user.email,
-        passward=user.passward
-    ).where(users.c.id==id)
-
-    return conn.execute(users.select()).fetchall()
-
-@user.get("/")
-async def delete_data():
-    conn.execute(users.delete())
-    return conn.execute(users.select()).fetchall()
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
